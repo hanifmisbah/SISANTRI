393f2900cf237b5443f6af3952ba88cc6f4f28c2
hanifmisbah <hanifmisbah97@gmail.com>
2021-11-19 06:22:31 +0700

    V.1

--- sisantri/adminpondok/views.py
+++ sisantri/adminpondok/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(req):
-    return render(req, 'adminpondok/index.html')
+    return render(req, 'adminpondok/dashboard.html')
+
+def profil(req):
+    return render(req, 'adminpondok/profil.html')
