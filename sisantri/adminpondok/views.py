5bb15c1d1b643226947612b8912a23478ce131cf
martabakhoror <jumanurrohman@gmail.com>
2021-11-22 13:12:37 +0700

    V.1

--- sisantri/adminpondok/views.py
+++ sisantri/adminpondok/views.py
@@ -10,28 +10,28 @@ def profil(req):
     return render(req, 'adminpondok/profil.html')
 
 def datasantri(req):
-    form = forms.Santri()
+    # form = forms.Santri()
     if req.POST:
-        form = forms.Santri(req.POST)
-        if form.is_valid():
-            form.save()
-        # models.Santri.objects.create(
-        #     nis=req.POST['nis'],
-        #     nama_santri=req.POST['nama_santri'],
-        #     tempat_lahir=req.POST['tempat_lahir'],
-        #     tanggal_lahir=req.POST['tanggal_lahir'],
-        #     jk=req.POST['jk'],
-        #     almt=req.POST['almt'],
-        #     telp=req.POST['telp'],
-        #     email=req.POST['email'],
-        #     ktgri=req.POST['ktgri'],
-        # )
+        # form = forms.Santri(req.POST)
+        # if form.is_valid():
+        #     form.save()
+        models.Santri.objects.create(
+            nis=req.POST['nis'],
+            nama_santri=req.POST['nama_santri'],
+            tempat_lahir=req.POST['tempat_lahir'],
+            tanggal_lahir=req.POST['tanggal_lahir'],
+            jk=req.POST['jk'],
+            almt=req.POST['almt'],
+            telp=req.POST['telp'],
+            email=req.POST['email'],
+            ktgri=req.POST['ktgri'],
+        )
         return redirect('/adminpondok')
 
     santri = models.Santri.objects.all()
     return render(req, 'adminpondok/datasantri.html', {
         'data' : santri,
-        'form' : form,
+        # 'form' : form,
     })
 
 def datapengajar(req):
