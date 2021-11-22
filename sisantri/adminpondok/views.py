b74e562c76ddae174af9c6096df18690af5cc5f6
hanifmisbah <hanifmisbah97@gmail.com>
2021-11-22 10:20:45 +0700

    V.1

--- sisantri/adminpondok/views.py
+++ sisantri/adminpondok/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from . import models, forms
 
 # Create your views here.
 def index(req):
@@ -8,10 +10,38 @@ def profil(req):
     return render(req, 'adminpondok/profil.html')
 
 def datasantri(req):
-    return render(req, 'adminpondok/datasantri.html')
+    form = forms.Santri()
+    if req.POST:
+        form = forms.Santri(req.POST)
+        if form.is_valid():
+            form.save()
+        # models.Santri.objects.create(
+        #     nis=req.POST['nis'],
+        #     nama_santri=req.POST['nama_santri'],
+        #     tempat_lahir=req.POST['tempat_lahir'],
+        #     tanggal_lahir=req.POST['tanggal_lahir'],
+        #     jk=req.POST['jk'],
+        #     almt=req.POST['almt'],
+        #     telp=req.POST['telp'],
+        #     email=req.POST['email'],
+        #     ktgri=req.POST['ktgri'],
+        # )
+        return redirect('/adminpondok')
+
+    santri = models.Santri.objects.all()
+    return render(req, 'adminpondok/datasantri.html', {
+        'data' : santri,
+        'form' : form,
+    })
 
 def datapengajar(req):
     return render(req, 'adminpondok/datapengajar.html')
 
 def datakitab(req):
     return render(req, 'adminpondok/datakitab.html')
+
+def quran(req):
+    return render(req, 'adminpondok/quran.html')
+
+def kitabkuning(req):
+    return render(req, 'adminpondok/kitabkuning.html')
