af11ec47283e92b578fad1c4ad67beefd889e0e0
Marissa Azzahro <80081163+marissa181112113@users.noreply.github.com>
2021-12-16 17:00:04 +0700

    V.1

--- sisantri/pengajar/views.py
+++ sisantri/pengajar/views.py
@@ -1,7 +1,15 @@
+from django.db import models
 from django.shortcuts import redirect, render
+from adminpondok import models as pond_models
 
 # Create your views here.
 
 
 def index(req):
-    return render(req, 'pengajar_dashboard.html')
+    data_santri = pond_models.Santri.objects.all()
+    print(data_santri)
+    konteks = { 'data' : data_santri } 
+    return render(req, 'pengajar_dashboard.html' , konteks)
+
+# def pengajar(req) :
+#     return render(req, 'pengajar_dashboard.html')
